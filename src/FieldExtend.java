2afd27927cc88c2505ae517eee73f50abdcbe522
Servbot52 <Servbot52@gmail.com>
2021-12-23 13:33:29 -0700

    Cleaned up FieldExtend

--- src/FieldExtend.java
+++ src/FieldExtend.java
@@ -1,16 +1,7 @@
 import javax.swing.JScrollPane;
 import javax.swing.JTextPane;
-import javax.swing.plaf.synth.ColorType;
 import javax.swing.text.DefaultHighlighter.DefaultHighlightPainter;
-import javax.swing.text.Highlighter.Highlight;
-import javax.swing.text.Position;
-import javax.swing.text.Utilities;
-import javax.swing.text.View;
-
-
-import javax.swing.text.AttributeSet.ColorAttribute;
 import javax.swing.text.BadLocationException;
-import javax.swing.JEditorPane;
 
 import java.awt.*;
 import java.awt.geom.Rectangle2D;
@@ -36,9 +27,9 @@ public class FieldExtend{
 	
 	void makeItVisable(int start, int length) {
 		try {
-			Rectangle2D rect = area.modelToView2D(start).getBounds2D();
-			
-			
+			Rectangle2D rect = area.modelToView2D(start);
+			rect.add(area.modelToView2D(start + length));
+			rect = rect.getBounds2D();
 			if (rect != null) {
 				Rectangle2D visibleRect = scroll.getVisibleRect().getBounds2D();
 				if(visibleRect.contains(rect) == false)
@@ -62,20 +53,5 @@ public class FieldExtend{
 		}
 		*/
 	}
-	/*
-	void getLocation(int theCharacter) {
-		View rootView = area.getUI().getRootView(area);
-		rootView.ge
-		area.getUI().getRootView(area).getv;
-		
-		int index;
-	    while ((index = rootView.getViewIndex(theCharacter, Position.Bias.Backward)) >= 0) {
-	    	rootView = rootView.getView(index);
-	    }
-		
-	}
-	*/
-	void rowHeights() {
-		area.getRootPane();
-	}
+	
 }
